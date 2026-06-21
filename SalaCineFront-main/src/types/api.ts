47a37514a6b8a6f05d33a539670db14b9422cf74
Ryhades <ryam_diniz@hotmail.com>
2026-06-21 03:08:35 -0300

    correção de mudança de link

--- SalaCineFront-main/src/types/api.ts
+++ SalaCineFront-main/src/types/api.ts
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+
+  // agora aponta diretamente pro backend na nuvem
+  baseURL: "https://projeto-manutencao-l7hy4nxg6-ryhades-4506s-projects.vercel.app/api",
   headers: {
     "Content-Type": "application/json",
   },
